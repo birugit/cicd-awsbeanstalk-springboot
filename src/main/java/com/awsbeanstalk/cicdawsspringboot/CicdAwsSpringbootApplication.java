0629792b537923156b071cc8abdf521495a222f3
Swamy Biru <Swamy-Biru@gap.com>
2019-10-17 23:29:45 -0700

    cicd aws beanstalk springboot

---
+++ src/main/java/com/awsbeanstalk/cicdawsspringboot/CicdAwsSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.awsbeanstalk.cicdawsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdAwsSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdAwsSpringbootApplication.class, args);
+	}
+
+}
